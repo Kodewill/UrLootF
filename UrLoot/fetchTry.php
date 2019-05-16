62db4bd1808de98735d508e47ed8f24f6df50f11
William <wilkingpa951@gmail.com>
2019-05-16 16:21:48 -0500

    Vendor
    gitIgnore modification for partner

--- UrLoot/fetchTry.php
+++ UrLoot/fetchTry.php
@@ -5,11 +5,20 @@ $password = "";
 $dbname = "urloot";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "INSERT INTO professor (professorName)
+VALUES ('Benjala Melami')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "algo";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?> 
