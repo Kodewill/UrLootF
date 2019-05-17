de87c7a2ff6c8d43ff5741e61d6057d0b3a62f08
William <wilkingpa951@gmail.com>
2019-05-17 03:49:53 -0500

    successful Query

--- UrLoot/app/Http/Controllers/Web/PageController.php
+++ UrLoot/app/Http/Controllers/Web/PageController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
 
 class PageController extends Controller
 {
     public function urloot(){
 
-        return view('web.post');
+        $professorNames = DB::table('professor')->get('professorName');
+
+        return view('web.post',compact('professorNames'));
     }
 }
--- UrLoot/resources/views/web/post.blade.php
+++ UrLoot/resources/views/web/post.blade.php
@@ -5,6 +5,20 @@
 <div class='container'>
     <div class = "col-md-8 col-md-offset-2">
         <h1>Examenes</h1>
+
+        @foreach($professorNames as $prof)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                {{$prof->professorName}}
+            </div>
+            <div class="panel-body">
+                <a href="#" class="pull-right">----</a>
+            </div>
+            
+
+        </div>
+        @endforeach
+        
     </div>
 </div>
 @endsection
